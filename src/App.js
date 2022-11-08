fd7a0f4050f21521698ba1611dfa8dccfe6aa4b4
doooox <dtopic12@gmail.com>
2022-11-08 18:35:00 +0100

    Set layout, AddCars component, set link to /cars

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import './App.css';
+import Routes from './Routes';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        <li>
+          <Link to="/cars">Cars</Link>
+        </li>
+      </ul>
+      <Routes/>
     </div>
   );
 }
---
+++ src/pages/AppCars.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AppCars = () => {
+  return (
+    <div>AppCars</div>
+  )
+}
+
+export default AppCars
